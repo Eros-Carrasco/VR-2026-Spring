533532fd700ab6feabbc81268942d26b964b565c
Ken Perlin <ken.perlin@gmail.com>
2026-06-14 14:16:20 -0400

    Added a version of the robot arm that moves entirely from IK.

--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -10,6 +10,7 @@ export default () => {
       enableSceneReloading: true,
       scenes: [ 
          { name: "robot"        , path: "./robot.js"        , public: true },
+         { name: "robotIK"      , path: "./robotIK.js"      , public: true },
          { name: "shapes"       , path: "./shapes.js"       , public: true },
          { name: "joints"       , path: "./joints.js"       , public: true },
          { name: "inputTest1"   , path: "./inputTest1.js"   , public: true },
